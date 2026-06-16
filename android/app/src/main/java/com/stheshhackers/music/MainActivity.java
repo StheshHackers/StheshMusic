aaaa8e7fca0e11c87951acde8f12959d394be7d3
Athabile Dinilanga <dinilangaathabile@gmail.com>
2026-06-16 13:16:31 +0200

    fix: register notification channel to unlock system settings

--- android/app/src/main/java/com/stheshhackers/music/MainActivity.java
+++ android/app/src/main/java/com/stheshhackers/music/MainActivity.java
@@ -1,5 +1,18 @@
 package com.stheshhackers.music;
 
 import com.getcapacitor.BridgeActivity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("media_playback_channel", "Music Playback", NotificationManager.IMPORTANCE_LOW);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) manager.createNotificationChannel(channel);
+        }
+    }
